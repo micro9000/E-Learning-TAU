b2c5793f011ee682c10f143a267d6fb3a66f962d
RANIEL GARCIA <ranielgarcia101@gmail.com>
2018-09-09 16:38:31 +0800

    Config: base_url ip address to localhost

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '//192.168.1.4/ELearning';
+$config['base_url'] = '//localhost/ELearning';
 
 /*
 |--------------------------------------------------------------------------
